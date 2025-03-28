f2c13e7c198e76c29d0fd11a8be3596416fd8cb2
Hristo Minkov <minkov.h@gmail.com>
2025-03-28 02:46:24 +0200

    Adjustments to main;

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def main():
-    print("Hello, World!")
+from utils import read_data, write_output
+
+
+def run(input_file_name: str) -> None:
+    D, R, T, resources, turns = read_data(input_file_name)
+    output = []
+
+    for idx, turn in enumerate(turns):
+        powered_buildings = 0
+
+        # purchase resources
+        purchased_resources = []
+        purchase_cost = 0
+        for res in resources.values():
+            if not res.activated and res.activation_cost + purchase_cost <= D:
+                purchase_cost += res.activation_cost
+                res.activated = True
+                purchased_resources.append(res.id)
+
+        if purchased_resources:
+            output.append(
+                f"{idx} {len(purchased_resources)} {' '.join(map(str, purchased_resources))}"
+            )
+
+    write_output(output, input_file_name)
 
 
 if __name__ == "__main__":
-    main()
+    for input_file_name in [
+        "0-demo",
+        # "1-thunberg",
+        # "2-attenborough",
+        # "3-goodall",
+        # "4-maathai",
+        # "5-carson",
+        # "6-earle",
+        # "7-mckibben",
+        # "8-shiva",
+    ]:
+        run(input_file_name)
